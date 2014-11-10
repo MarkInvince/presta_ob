1fe7371b7b1e8f62e69b717da0d4116cd7740330
Romik <Romik19.09@gmail.com>
2014-11-10 11:04:01 +0200

    new commit

--- theme/modules/quotes/sql/install.php
+++ theme/modules/quotes/sql/install.php
@@ -7,9 +7,9 @@ $sql = array();
     PRIMARY KEY  (`id_quotes`)
 ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';*/
 
-$sql[] = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."quotes` (
+$sql[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'quotes` (
           `id_request` int(10) unsigned NOT NULL AUTO_INCREMENT,
-          `id_shop` int(11) unsigned NOT NULL DEFAULT '1',
+          `id_shop` int(11) unsigned NOT NULL DEFAULT "1",
           `id_carrier` int(10) unsigned NOT NULL,
           `id_lang` int(10) unsigned NOT NULL,
           `id_customer` int(10) unsigned NOT NULL,
@@ -17,18 +17,18 @@ $sql[] = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."quotes` (
           `date_add` datetime NOT NULL,
           `date_upd` datetime NOT NULL,
           PRIMARY KEY (`id_request`),
-        ) ENGINE="._MYSQL_ENGINE_."  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;";
+        ) ENGINE='._MYSQL_ENGINE_.'  DEFAULT CHARSET=utf8 AUTO_INCREMENT ;';
 
-$sql[] = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."quotes_product` (
+$sql[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'quotes_product` (
           `id_cart` int(10) unsigned NOT NULL,
           `id_product` int(10) unsigned NOT NULL,
-          `id_shop` int(10) unsigned NOT NULL DEFAULT '1',
+          `id_shop` int(10) unsigned NOT NULL DEFAULT "1",
           `id_product_attribute` int(10) unsigned DEFAULT NULL,
-          `quantity` int(10) unsigned NOT NULL DEFAULT '0',
+          `quantity` int(10) unsigned NOT NULL DEFAULT "0",
           `date_add` datetime NOT NULL,
           KEY `cart_product_index` (`id_cart`,`id_product`),
           KEY `id_product_attribute` (`id_product_attribute`)
-        ) ENGINE="._MYSQL_ENGINE_." DEFAULT CHARSET=utf8;";
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
 
 foreach ($sql as $query)
     if (Db::getInstance()->execute($query) == false)
